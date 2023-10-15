85b2e44797a4d40b881eef26572db254efab31de
fitsaleem <sherohero33@gmail.com>
2023-10-15 17:01:56 +0500

    api curd operations

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://fitsaleem:saleemraza@cluster0.byc6obh.mongodb.net/mernecomerce');
+  await mongoose.connect(process.env.MONGO_URI);
   console.log('database connected')
 }
 
