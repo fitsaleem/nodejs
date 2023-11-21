38fbe67ecaec6f2cea947222a7a082d275a93f3d
fitsaleem <sherohero33@gmail.com>
2023-11-21 16:32:20 +0500

    added login api

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,8 +1,7 @@
 import User from "../models/usermodel.js";
 import bcryptjs from "bcryptjs";
-import { errorHandler } from '../utils/error.js';
-import jwt from 'jsonwebtoken';
-
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -29,20 +28,23 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
   try {
-
     const validUser = await User.findOne({ email }); //you can also write this {email: email}
 
-   //  if(!validUser) return res.status(401).json({message: "Invalid email"})
-    if (!validUser) return next (errorHandler(404,"user not  found"))
+    //  if(!validUser) return res.status(401).json({message: "Invalid email"})
+    if (!validUser) return next(errorHandler(404, "user not  found"));
 
-    const validPassword=bcryptjs.compareSync(password,validUser.password);
-    if (!validPassword) return next(errorHandler(401,"wrong credentials"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credentials"));
 
-    const token=jwt.sign({id: validUser._id},process.env.MY_SECRET_KEY);
+    const token = jwt.sign({ id: validUser._id }, process.env.MY_SECRET_KEY);
 
-    res.cookies('access_token',token,{httpOnly: true});
+    // for removing password becouse we want to show password on clinet side
+    const { password: hashedPassword, ...rest } = validUser._doc;
 
-
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
